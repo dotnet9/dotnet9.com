3f4c212ad916db0ce739a2dfdcacf388d0a648f3
Dotnet9(dotnet9.com) <1012434131@qq.com>
2022-02-02 20:11:20 +0800

    delete unused files;

--- src/Program.cs
+++ src/Program.cs
@@ -1,13 +1,8 @@
-using Dotnet9.Data;
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
 
 var app = builder.Build();
 
