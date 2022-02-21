4417f91b8be303d1908d543fe59e015679b4bfd7
dotnet9 <1012434131@qq.com>
2022-02-21 17:49:30 +0800

    Delete unused files, add public layout and management layout;

--- src/Dotnet9.Tools/Program.cs
+++ src/Dotnet9.Tools/Program.cs
@@ -1,21 +1,13 @@
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
-using Dotnet9.Tools.Data;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-}
+if (!app.Environment.IsDevelopment()) app.UseExceptionHandler("/Error");
 
 
 app.UseStaticFiles();
