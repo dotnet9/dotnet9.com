a1996de2cc92db6fcb077043187afeca02d05f56
Dotnet9 <1012434131@qq.com>
2022-03-07 15:29:06 +0800

    add html encoder;

--- src/Dotnet9.Tools.Web/Program.cs
+++ src/Dotnet9.Tools.Web/Program.cs
@@ -1,4 +1,6 @@
 using System.Globalization;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
 using Dotnet9.Tools.Web.Services;
 using Dotnet9.Tools.Web.Utils;
 
@@ -12,6 +14,7 @@ builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 
 builder.Services.AddTransient<CommentService>();
+builder.Services.AddSingleton(HtmlEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.CjkUnifiedIdeographs));
 
 var app = builder.Build();
 
