5b707f07b726604e6477ff2a7c04a31c744100af
Dotnet9(dotnet9.com) <1012434131@qq.com>
2022-03-29 22:42:01 +0800

    add 404 page;

---
+++ src/Dotnet9.Web/Controllers/ErrorController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Dotnet9.Web.Controllers;
+
+public class ErrorController : Controller
+{
+    [Route("/error/{0}")]
+    public IActionResult Index()
+    {
+        if (Response.StatusCode == StatusCodes.Status404NotFound)
+        {
+            return View("/Views/Error/Index.cshtml");
+        }
+        return View();
+    }
+}
--- src/Dotnet9.Web/Program.cs
+++ src/Dotnet9.Web/Program.cs
@@ -16,6 +16,7 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment()) app.UseExceptionHandler("/Home/Error");
+app.UseStatusCodePagesWithReExecute("/error/{0}");
 
 app.UseStaticFiles();
 
