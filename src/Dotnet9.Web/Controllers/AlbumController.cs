5f2fe4256c9a1796aac92664000106a5f6a82d07
Dotnet9(dotnet9.com) <1012434131@qq.com>
2022-03-29 23:09:51 +0800

    add album controller;

---
+++ src/Dotnet9.Web/Controllers/AlbumController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Dotnet9.Web.Controllers
+{
+    public class AlbumController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
