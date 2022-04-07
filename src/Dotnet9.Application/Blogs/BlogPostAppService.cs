de3c4a6bd678223fabbc448c120d0129a859b209
Dotnet9 <1012434131@qq.com>
2022-04-07 14:17:44 +0800

    display same album or category blog post ;

--- src/Dotnet9.Application/Blogs/BlogPostAppService.cs
+++ src/Dotnet9.Application/Blogs/BlogPostAppService.cs
@@ -1,18 +1,26 @@
 ﻿using AutoMapper;
 using Dotnet9.Application.Contracts.Blogs;
+using Dotnet9.Core;
+using Dotnet9.Domain.Albums;
 using Dotnet9.Domain.Blogs;
+using Dotnet9.Domain.Categories;
 using Dotnet9.Domain.Repositories;
 
 namespace Dotnet9.Application.Blogs;
 
 public class BlogPostAppService : IBlogPostAppService
 {
+    private readonly IAlbumRepository _albumRepository;
     private readonly IBlogPostRepository _blogPostRepository;
+    private readonly ICategoryRepository _categoryRepository;
     private readonly IMapper _mapper;
 
-    public BlogPostAppService(IBlogPostRepository blogPostRepository, IMapper mapper)
+    public BlogPostAppService(IBlogPostRepository blogPostRepository, IAlbumRepository albumRepository,
+        ICategoryRepository categoryRepository, IMapper mapper)
     {
         _blogPostRepository = blogPostRepository;
+        _albumRepository = albumRepository;
+        _categoryRepository = categoryRepository;
         _mapper = mapper;
     }
 
@@ -31,6 +39,31 @@ public class BlogPostAppService : IBlogPostAppService
         var nextPost = await _blogPostRepository.GetBlogPostBriefAsync(
             x => x.CreateDate > blogPostWithDetails.CreateDate,
             x => x.CreateDate, SortDirectionKind.Ascending);
+        if (!blogPostWithDetails.AlbumNames.IsNullOrEmpty())
+        {
+            var album = await _albumRepository.GetAsync(x => x.Name == blogPostWithDetails.AlbumNames![0]);
+            if (album != null)
+            {
+                var sameAlbumPost = await _blogPostRepository.SelectBlogPostBriefAsync(4, 1,
+                    x => x.Albums != null && x.Albums.Any(d => d.AlbumId == album.Id), x => x.CreateDate,
+                    SortDirectionKind.Descending);
+                vm.SameAlbumBlogPosts = _mapper.Map<List<BlogPostBrief>, List<BlogPostBriefDto>>(sameAlbumPost.Item1);
+            }
+        }
+
+        if (!blogPostWithDetails.CategoryNames.IsNullOrEmpty())
+        {
+            var category =
+                await _categoryRepository.GetAsync(x => x.Name == blogPostWithDetails.CategoryNames![0]);
+            if (category != null)
+            {
+                var sameCategoryPost = await _blogPostRepository.SelectBlogPostBriefAsync(4, 1,
+                    x => x.Categories != null && x.Categories.Any(d => d.CategoryId == category.Id), x => x.CreateDate,
+                    SortDirectionKind.Descending);
+                vm.SameCategoryBlogPosts =
+                    _mapper.Map<List<BlogPostBrief>, List<BlogPostBriefDto>>(sameCategoryPost.Item1);
+            }
+        }
 
         if (previewPost != null) vm.PreviewBlogPost = _mapper.Map<BlogPostBrief, BlogPostBriefDto>(previewPost);
 
