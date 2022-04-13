295b7e339d3f6ce1f17496d41804b15852bc704f
Dotnet9 <1012434131@qq.com>
2022-04-13 11:27:09 +0800

    add feature to contact side bar;

---
+++ src/Dotnet9.Web/ViewModels/Abouts/ContactSidebarViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Dotnet9.Web.ViewModels.Abouts;
+
+public class ContactSidebarViewModel
+{
+    public int TotalPostCount { get; set; }
+    public DateTime? LatestPostTime { get; set; }
+}
