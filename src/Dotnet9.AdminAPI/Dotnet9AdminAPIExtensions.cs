a084c11f82b045809b837af02a140a460a6317fb
Dotnet9 <1012434131@qq.com>
2022-04-20 18:04:10 +0800

    add Dotnet9.AdminAPI;

---
+++ src/Dotnet9.AdminAPI/Dotnet9AdminAPIExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dotnet9.AdminAPI;
+
+public static class Dotnet9AdminAPIExtensions
+{
+    public static void AddAdminAPI<T>(this WebApplicationBuilder app) where T : DbContext
+    {
+
+    }
+}
