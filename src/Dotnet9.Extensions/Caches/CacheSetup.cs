1c96ac4da7eef5cfd102364bc4ebc936841bd41e
Dotnet9 <1012434131@qq.com>
2022-04-20 18:03:52 +0800

    add Dotnet9.Extensions;

---
+++ src/Dotnet9.Extensions/Caches/CacheSetup.cs
@@ -0,0 +1,31 @@
+﻿using Dotnet9.Application.Contracts.Caches;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Caching.StackExchangeRedis;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Dotnet9.Extensions.Caches;
+
+public static class CacheSetup
+{
+    public static void AddCacheSetup(this IServiceCollection services, CacheConfig cacheConfig)
+    {
+        services.AddMemoryCache();
+        if (cacheConfig.IsRedis)
+        {
+            services.AddSingleton(typeof(ICacheService), new RedisCacheService(new RedisCacheOptions
+            {
+                Configuration = cacheConfig.RedisConnection,
+                InstanceName = cacheConfig.InstanceName
+            }));
+        }
+        else
+        {
+            services.AddSingleton<IMemoryCache>(factory =>
+            {
+                var cache = new MemoryCache(new MemoryCacheOptions());
+                return cache;
+            });
+            services.AddSingleton<ICacheService, MemoryCacheService>();
+        }
+    }
+}
---
+++ src/Dotnet9.Extensions/Dotnet9Extension.cs
@@ -0,0 +1,13 @@
+﻿using Dotnet9.Extensions.Caches;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+
+namespace Dotnet9.Extensions;
+
+public static class Dotnet9Extension
+{
+    public static void AddExtensions(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddCacheSetup(builder.Configuration.GetSection("Cache").Get<CacheConfig>()!);
+    }
+}
