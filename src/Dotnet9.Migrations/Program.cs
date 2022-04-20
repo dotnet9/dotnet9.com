6400e082bc3d144d8ca34ed3f3be04d983cbe70c
Dotnet9 <1012434131@qq.com>
2022-04-20 16:54:43 +0800

    add Dotnet9.Migrations;

---
+++ src/Dotnet9.Migrations/Program.cs
@@ -0,0 +1,10 @@
+﻿using Dotnet9.EntityFrameworkCore.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+Console.WriteLine(args[0]);
+Console.WriteLine("开始迁移");
+var optionsBuilder = new DbContextOptionsBuilder<Dotnet9DbContext>();
+optionsBuilder.UseMySql(args[0], ServerVersion.AutoDetect(args[0]));
+var context = new Dotnet9DbContext(optionsBuilder.Options);
+await context.Database.MigrateAsync();
+Console.WriteLine("迁移成功");
