e903e917864a2fe0e83b7e8a16defd3bddf227f7
Dotnet9(dotnet9.com) <1012434131@qq.com>
2022-05-13 15:12:12 +0800

    fix initialize the authentication rules of the administrator account;

--- src/Dotnet9.Admin/src/views/auth/InitAccount.vue
+++ src/Dotnet9.Admin/src/views/auth/InitAccount.vue
@@ -1,85 +1,122 @@
 <template>
-    <div class="d-login">
-        <div class="d-login-box" v-loading="loading">
-            <div class="d-login-header">
-                <h1>初始化后台账号</h1>
-            </div>
-            <el-form :model="formData" label-width="70px" :rules="rules" ref="form" label-position="top">
-                <el-form-item label="账号" prop="account">
-                    <el-input placeholder="输入账号" v-model="formData.account"></el-input>
-                </el-form-item>
-                <el-form-item label="邮箱" prop="email">
-                    <el-input placeholder="输入邮箱" v-model="formData.email"></el-input>
-                </el-form-item>
-                <el-form-item label="密码" prop="password">
-                    <el-input placeholder="输入密码" v-model="formData.password" show-password></el-input>
-                </el-form-item>
-                <el-form-item label>
-                    <el-button type="primary" class="w_100" @click="submitHandler()">创建</el-button>
-                </el-form-item>
-            </el-form>
-        </div>
+  <div class="d-login">
+    <div class="d-login-box" v-loading="loading">
+      <div class="d-login-header">
+        <h1>初始化后台账号</h1>
+      </div>
+      <el-form
+        :model="formData"
+        label-width="70px"
+        :rules="rules"
+        ref="form"
+        label-position="top"
+      >
+        <el-form-item label="账号" prop="account">
+          <el-input
+            placeholder="输入账号"
+            v-model="formData.account"
+          ></el-input>
+        </el-form-item>
+        <el-form-item label="邮箱" prop="email">
+          <el-input placeholder="输入邮箱" v-model="formData.email"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+          <el-input
+            placeholder="输入密码"
+            v-model="formData.password"
+            show-password
+          ></el-input>
+        </el-form-item>
+        <el-form-item label>
+          <el-button type="primary" class="w_100" @click="submitHandler()"
+            >创建</el-button
+          >
+        </el-form-item>
+      </el-form>
     </div>
+  </div>
 </template>
 
 <script lang="ts" setup>
-import { reactive, ref, onMounted } from 'vue';
-import { useRouter } from 'vue-router';
-import { ElForm, ElMessage } from 'element-plus'
-import { post, get } from 'shared/http/HttpClient'
-
+import { reactive, ref, onMounted } from "vue";
+import { useRouter } from "vue-router";
+import { ElForm, ElMessage } from "element-plus";
+import { post, get } from "shared/http/HttpClient";
 
 const formData = reactive({
-    account: "",
-    email: '',
-    password: ""
+  account: "",
+  email: "",
+  password: "",
 });
 const loading = ref<boolean>(false);
+var checkEmail = (rule, value, callback) => {
+  const regEmail = /^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+  if (regEmail.test(value)) {
+    return callback();
+  }
+  callback(new Error("请输入合法的邮箱"));
+};
 const rules = {
-    account: [
-        {
-            required: true,
-            message: "账号不能为空",
-            trigger: 'blur'
-        },
-    ],
-    email: [
-        {
-            required: true,
-            message: '邮箱不能为空',
-            trigger: 'blur'
-        }
-    ],
-    pwd: [
-        {
-            required: true,
-            message: "密码不能为空",
-            trigger: 'blur'
-        },
-    ],
+  account: [
+    {
+      required: true,
+      message: "账号不能为空",
+      trigger: "blur",
+    },
+    {
+        min: 3,
+        max: 10,
+        message: '账号的长度在 3 ~ 10个字符之间',
+        trigger: 'blue'
+    }
+  ],
+  email: [
+    {
+      required: true,
+      message: "邮箱不能为空",
+      trigger: "blur",
+    },
+    {
+        validator: checkEmail,
+        trigger: 'blur'
+    }
+  ],
+  password: [
+    {
+      required: true,
+      message: "密码不能为空",
+      trigger: "blur",
+    },
+    {
+        min: 6,
+        max: 15,
+        message: '密码的长度在 6 ~ 15个字符之间',
+        trigger: 'blue'
+    }
+  ],
 };
 const form = ref<InstanceType<typeof ElForm>>();
 
 const router = useRouter();
 
 const submitHandler = () => {
-    form.value.validate((isvalid) => {
-        if (isvalid) {
-            loading.value = true;
-            post('/api/account/createAdminAccount', formData).then(_ => {
-                ElMessage({
-                    message: '初始化管理员账号成功',
-                    type: 'success'
-                })
-                router.replace('/login')
-            }).finally(() => loading.value = false)
-        }
-    })
-
-}
+  form.value.validate((isvalid) => {
+    if (isvalid) {
+      loading.value = true;
+      post("/api/account/createAdminAccount", formData)
+        .then((_) => {
+          ElMessage({
+            message: "初始化管理员账号成功",
+            type: "success",
+          });
+          router.replace("/login");
+        })
+        .finally(() => (loading.value = false));
+    }
+  });
+};
 
 onMounted(() => {
-    get('/api/account/checkLogin', {})
-})
-
+  get("/api/account/checkLogin", {});
+});
 </script>
