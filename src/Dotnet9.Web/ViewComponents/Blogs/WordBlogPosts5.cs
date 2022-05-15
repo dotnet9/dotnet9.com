355c8dede9d65c1bdc9089c4520d5fbd4329a2e7
Dotnet9(dotnet9.com) <1012434131@qq.com>
2022-05-15 23:54:57 +0800

    sort home;

--- src/Dotnet9.Web/ViewComponents/Blogs/WordBlogPosts5.cs
+++ src/Dotnet9.Web/ViewComponents/Blogs/WordBlogPosts5.cs
@@ -27,7 +27,7 @@ public class WordBlogPosts5 : ViewComponent
 
         var categories = new[]
         {
-            "dotnet-more-dotnet-other", "dotnet-web-api", "dotnet-web-mvc", "dotnet-web-blazor", "dotnet-mobile-maui"
+            "dotnet-more-dotnet-other", "dotnet-web-api", "dotnet-web-blazor", "dotnet-mobile-maui", "dotnet-web-mvc"
         };
 
         cacheData = new WordBlogPostsViewModel
