b010ab2d53bbb8c82016646a3171f595d3fdbcb0
Dotnet9(dotnet9.com) <1012434131@qq.com>
2022-05-21 21:26:44 +0800

    Update ActionLogConsts.cs

--- src/Dotnet9.Domain.Shared/ActionLogs/ActionLogConsts.cs
+++ src/Dotnet9.Domain.Shared/ActionLogs/ActionLogConsts.cs
@@ -3,16 +3,16 @@
 public static class ActionLogConsts
 {
     public const int MaxUIdLength = 40;
-    public const int MaxUALength = 256;
+    public const int MaxUALength = 512;
     public const int MaxOSLength = 32;
     public const int MaxBrowserLength = 32;
-    public const int MaxRefererLength = 48;
-    public const int MaxAccessName = 48;
-    public const int MaxOriginalLength = 256;
+    public const int MaxRefererLength = 128;
+    public const int MaxAccessName = 128;
+    public const int MaxOriginalLength = 512;
     public const int MaxIPLength = 32;
-    public const int MaxUrlLength = 256;
+    public const int MaxUrlLength = 512;
     public const int MaxControllerLength = 128;
     public const int MaxActionLength = 128;
     public const int MaxMethodLength = 8;
-    public const int MaxArgumentsLength = 256;
+    public const int MaxArgumentsLength = 512;
 }
