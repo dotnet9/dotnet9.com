b59cf12015f02bc19003ba66eb24f4c718b0c931
Dotnet9(dotnet9.com) <1012434131@qq.com>
2022-05-22 01:28:29 +0800

    添加调试sql打印；

--- src/Dotnet9.Web/ServiceExtensions/DbSetup.cs
+++ src/Dotnet9.Web/ServiceExtensions/DbSetup.cs
@@ -5,10 +5,17 @@ namespace Dotnet9.Web.ServiceExtensions;
 
 public static class DbSetup
 {
+    public static readonly ILoggerFactory efLogger = LoggerFactory.Create(builder =>
+    {
+        builder.AddFilter((category, level) =>
+            category == DbLoggerCategory.Database.Command.Name && level == LogLevel.Information).AddConsole();
+    });
+
     public static void AddDbSetup(this IServiceCollection services, string connectionStr)
     {
         services.AddDbContextPool<Dotnet9DbContext>(option =>
-            option.UseNpgsql(connectionStr));
+            option.UseNpgsql(connectionStr)
+                .UseLoggerFactory(efLogger));
         AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
     }
 }
