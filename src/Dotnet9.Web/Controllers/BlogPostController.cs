57116e321763ce707ce362cd77c2445522355842
Dotnet9(dotnet9.com) <1012434131@qq.com>
2022-05-26 00:06:35 +0800

    修改错别字：公共查询文章接口的关键字参数 keyboard=>keyword；

--- src/Dotnet9.Web/Controllers/BlogPostController.cs
+++ src/Dotnet9.Web/Controllers/BlogPostController.cs
@@ -1,7 +1,4 @@
-﻿using System.Linq.Expressions;
-using System.Net;
-using System.Text.RegularExpressions;
-using AutoMapper;
+﻿using AutoMapper;
 using Dotnet9.Application.Contracts.Blogs;
 using Dotnet9.Application.Contracts.Caches;
 using Dotnet9.Core;
@@ -9,11 +6,16 @@ using Dotnet9.Domain.Blogs;
 using Dotnet9.Domain.Categories;
 using Dotnet9.Domain.Repositories;
 using Dotnet9.Web.ViewModels.Blogs;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq.Expressions;
+using System.Net;
+using System.Text.RegularExpressions;
 
 namespace Dotnet9.Web.Controllers;
 
-public class BlogPostController : Controller
+[Authorize]
+public partial class BlogPostController : Controller
 {
     private readonly IBlogPostAppService _blogPostAppService;
     private readonly IBlogPostRepository _blogPostRepository;
@@ -23,12 +25,12 @@ public class BlogPostController : Controller
 
     private readonly Dictionary<LoadMoreKind, string> _kindKeys = new()
     {
-        { LoadMoreKind.Dotnet, "dotnet" },
-        { LoadMoreKind.Front, "Large-front-end" },
-        { LoadMoreKind.Database, "database" },
-        { LoadMoreKind.MoreLanguage, "more-language" },
-        { LoadMoreKind.Course, "course" },
-        { LoadMoreKind.Other, "other" }
+        {LoadMoreKind.Dotnet, "dotnet"},
+        {LoadMoreKind.Front, "Large-front-end"},
+        {LoadMoreKind.Database, "database"},
+        {LoadMoreKind.MoreLanguage, "more-language"},
+        {LoadMoreKind.Course, "course"},
+        {LoadMoreKind.Other, "other"}
     };
 
     private readonly IMapper _mapper;
@@ -50,6 +52,7 @@ public class BlogPostController : Controller
 
     [HttpGet]
     [Route("/{year}/{month}/{slug?}")]
+    [AllowAnonymous]
     public async Task<IActionResult> Index(int year, int month, string? slug)
     {
         if (slug.IsNullOrWhiteSpace()) return NotFound();
@@ -68,6 +71,7 @@ public class BlogPostController : Controller
 
     [HttpGet]
     [Route("/recommend")]
+    [AllowAnonymous]
     public async Task<IActionResult> Recommend()
     {
         const string cacheKey = $"{nameof(BlogPostController)}-{nameof(Recommend)}";
@@ -82,6 +86,7 @@ public class BlogPostController : Controller
 
     [HttpGet]
     [Route("/latest")]
+    [AllowAnonymous]
     public async Task<IActionResult> LoadLatest(string kind = "", int page = 1)
     {
         var cacheKey = $"{nameof(BlogPostController)}-{nameof(LoadLatest)}-{kind}-{page}";
@@ -90,7 +95,7 @@ public class BlogPostController : Controller
 
         var loadKind = LoadMoreKind.Dotnet;
         if (Enum.TryParse(typeof(LoadMoreKind), kind, out var enumKind))
-            loadKind = (LoadMoreKind)Enum.Parse(typeof(LoadMoreKind), kind);
+            loadKind = (LoadMoreKind) Enum.Parse(typeof(LoadMoreKind), kind);
 
         Expression<Func<BlogPost, bool>> whereLambda = x => x.Id > 0;
         if (_kindKeys.ContainsKey(loadKind))
@@ -115,21 +120,27 @@ public class BlogPostController : Controller
         return PartialView(cacheData);
     }
 
-    [HttpGet]
-    [Route("/q")]
-    public async Task<IActionResult> Query(string? keyboard, int p = 1)
+    [HttpGet("/q")]
+    [AllowAnonymous]
+    public async Task<IActionResult> Query(string? keyword, int p = 1)
     {
-        var cacheKey = $"{nameof(BlogPostController)}-{nameof(Query)}-{keyboard}-{p}";
+        var cacheKey = $"{nameof(BlogPostController)}-{nameof(Query)}-{keyword}-{p}";
         var cacheData = await _cacheService.GetAsync<QueryViewModel>(cacheKey);
         if (cacheData != null) return PartialView(cacheData);
 
-        Expression<Func<BlogPost, bool>> whereLambda = x => x.Id > 0;
-        if (!keyboard.IsNullOrWhiteSpace())
+        Expression<Func<BlogPost, bool>> whereLambda;
+        if (keyword.IsNullOrWhiteSpace())
         {
-            var queryStr = WebUtility.UrlDecode(keyboard);
+            whereLambda = x => x.Id > 0;
+        }
+        else
+        {
+            var queryStr = WebUtility.UrlDecode(keyword);
             whereLambda = x =>
                 Regex.IsMatch(x.Title, queryStr!) ||
+                Regex.IsMatch(x.Slug, queryStr!) ||
                 (x.Original != null && Regex.IsMatch(x.Original, queryStr!)) ||
+                Regex.IsMatch(x.BriefDescription, queryStr!) ||
                 Regex.IsMatch(x.Content, queryStr!);
         }
 
@@ -138,7 +149,7 @@ public class BlogPostController : Controller
 
         cacheData = new QueryViewModel
         {
-            Query = keyboard,
+            Query = keyword,
             PageIndex = p,
             PageCount = (queryResult.Item2 + 8 - 1) / 8,
             Total = queryResult.Item2
