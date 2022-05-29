2f9a3ba7107a6e7bb8c55f1e4ddbcaaf6f130c1f
Dotnet9(dotnet9.com) <1012434131@qq.com>
2022-05-29 22:41:33 +0800

    格式化编辑页面；

--- src/Dotnet9.Admin/src/views/blogs/posts/PostWrite.vue
+++ src/Dotnet9.Admin/src/views/blogs/posts/PostWrite.vue
@@ -30,16 +30,19 @@
             v-for="(item, i) in categoryItems"
             :key="i"
             name="categories"
-          >{{ item.name }}</el-checkbox>
+            >{{ item.name }}</el-checkbox
+          >
         </el-checkbox-group>
       </el-form-item>
       <el-form-item label="标签" prop="tags">
         <tag-box v-model="formModel.tags"></tag-box>
       </el-form-item>
       <el-form-item label="封面图">
-        <div @click="selectImage()" style="display: inline-block;">
-          <el-button type="info" v-if="!formModel.cover">上传一张封面图</el-button>
-          <img style="width: 200px;" :src="formModel.cover" />
+        <div @click="selectImage()" style="display: inline-block">
+          <el-button type="info" v-if="!formModel.cover"
+            >上传一张封面图</el-button
+          >
+          <img style="width: 200px" :src="formModel.cover" />
         </div>
       </el-form-item>
       <el-form-item>
@@ -62,174 +65,187 @@ import TagBox from "../../../components/TagBox.vue";
 import { CategoryItem } from "../categories/CategoryModel";
 import Reprint from "./Reprint.vue";
 
-
 interface articleItem {
-  id: number
-  title: string
-  content: string
-  markdown: string
-  categories: number[],
-  cover: string,
-  tags: string[]
+  id: number;
+  title: string;
+  content: string;
+  markdown: string;
+  categories: number[];
+  cover: string;
+  tags: string[];
 }
 
 const formModel = ref<articleItem>({
   id: 0,
   categories: [],
-  title: '',
-  content: '',
-  markdown: '',
-  cover: '',
-  tags: []
-})
+  title: "",
+  content: "",
+  markdown: "",
+  cover: "",
+  tags: [],
+});
 
-const coverBase64 = ref<string>("")
-const title = ref("新建")
-const loading = ref(false)
-const categoryItems = ref<CategoryItem[]>()
+const coverBase64 = ref<string>("");
+const title = ref("新建");
+const loading = ref(false);
+const categoryItems = ref<CategoryItem[]>();
 const loadCategories = async () => {
-  var res = await get<CategoryItem[]>('/api/category/list', {})
+  var res = await get<CategoryItem[]>("/api/category/list", {});
   categoryItems.value = res;
-}
+};
 
 onMounted(async () => {
   await loadCategories();
   await loadArticleData();
-})
+});
 
-var formRef = ref<InstanceType<typeof ElForm>>()
+var formRef = ref<InstanceType<typeof ElForm>>();
 
 const submitForm = async () => {
-  console.log(formModel.value)
+  console.log(formModel.value);
   formRef.value?.validate(async (valid) => {
     if (valid) {
       try {
         loading.value = true;
-        await post('/admin/post/addorupdate', formModel.value)
+        await post("/admin/post/addorupdate", formModel.value);
         ElMessage({
-          type: 'success',
-          message: '保存成功'
-        })
+          type: "success",
+          message: "保存成功",
+        });
         formRef.value?.resetFields();
-        router.push('/admin/post')
+        router.push("/admin/post");
       } finally {
-        loading.value = false
+        loading.value = false;
       }
     }
   });
-}
+};
 
 const rules = {
-  title: [{
-    required: true, message: '标题不能为空',
-    trigger: 'blur'
-  }],
-  content: [{
-    required: true, message: '内容不能为空'
-  }],
-  categories: [{
-    required: true, message: '分类至少选择一个', trigger: 'blur'
-  }]
-}
+  title: [
+    {
+      required: true,
+      message: "标题不能为空",
+      trigger: "blur",
+    },
+  ],
+  content: [
+    {
+      required: true,
+      message: "内容不能为空",
+    },
+  ],
+  categories: [
+    {
+      required: true,
+      message: "分类至少选择一个",
+      trigger: "blur",
+    },
+  ],
+};
 
 var router = useRouter();
 var route = useRoute();
 
 const back = () => {
   router.back();
-}
+};
 
-console.log('params:', route.query.id)
+console.log("params:", route.query.id);
 
-router.afterEach(guard => {
-  console.log('id:' + route.query.id)
+router.afterEach((guard) => {
+  console.log("id:" + route.query.id);
   if (!route.query.id) {
     reSet();
   }
-})
+});
 
 const reSet = () => {
   formRef.value?.resetFields();
   setTimeout(() => {
     formRef.value?.clearValidate();
   }, 200);
-}
-
+};
 
 const loadArticleData = async () => {
   try {
-    loading.value = true
+    loading.value = true;
     if (route.query.id) {
-      title.value = "编辑"
-      var res = await get<
-      { title: string, id: number, content: string, categories: { id: number, cateName: string }[], tags: string[], cover: string, markDown: string }>
-        ('/admin/post/get', { id: route.query.id })
-      console.log('res:', res)
+      title.value = "编辑";
+      var res = await get<{
+        title: string;
+        id: number;
+        content: string;
+        categories: { id: number; cateName: string }[];
+        tags: string[];
+        cover: string;
+        markDown: string;
+      }>("/admin/post/get", { id: route.query.id });
+      console.log("res:", res);
       formModel.value = {
         title: res.title,
         content: res.content,
-        categories: res.categories.map(a => a.id),
+        categories: res.categories.map((a) => a.id),
         id: res.id,
         tags: res.tags,
         cover: res.cover,
-        markdown: res.markDown
-      }
-      console.log('formModel:', formModel.value.tags)
-
-
+        markdown: res.markDown,
+      };
+      console.log("formModel:", formModel.value.tags);
     }
   } finally {
-    loading.value = false
+    loading.value = false;
   }
+};
 
-}
-
-const showCropper = ref(false)
+const showCropper = ref(false);
 
 const cropperSelect = (data: { base64: string }) => {
   coverBase64.value = data.base64;
   let loading = ElLoading.service({
-    text: '上传图片中'
+    text: "上传图片中",
+  });
+  post("/uploadbase64", {
+    base64: data.base64,
   })
-  post('/uploadbase64', {
-    base64: data.base64
-  }).then((res: any) => {
-    console.log(res)
-    formModel.value.cover = res.url
-  }).finally(() => {
-    loading.close();
-  })
-}
+    .then((res: any) => {
+      console.log(res);
+      formModel.value.cover = res.url;
+    })
+    .finally(() => {
+      loading.close();
+    });
+};
 
 const selectImage = () => {
   showCropper.value = true;
-}
+};
 
 const spiderComplate = (html: string) => {
-  console.log('回调')
+  console.log("回调");
   formModel.value.content = html;
-}
+};
 
 const uploadImage = (event: any, insertImage: any, files: File[]) => {
-  console.log(files)
-  files.map(file => {
-    console.log(file.name)
+  console.log(files);
+  files.map((file) => {
+    console.log(file.name);
     var formData = new FormData();
-    formData.append('img', file);
-    upload('/upload', formData).then((res: any) => {
+    formData.append("img", file);
+    upload("/upload", formData).then((res: any) => {
       insertImage({
-        url: res.url
-      })
-    })
-  })
-}
+        url: res.url,
+      });
+    });
+  });
+};
 
 const mdChange = (text: string, html: string) => {
-  console.log(text)
-  console.log(html)
-  formModel.value.content = html
-  formModel.value.markdown = text
-}
+  console.log(text);
+  console.log(html);
+  formModel.value.content = html;
+  formModel.value.markdown = text;
+};
 </script>
 
 <style>
