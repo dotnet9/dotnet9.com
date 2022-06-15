d8dd62d0ca3fa0cdc7c66c9eba33528580f2f6c8
Dotnet9(dotnet9.com) <1012434131@qq.com>
2022-06-16 00:22:19 +0800

    添加maui后台空项目；

---
+++ src/Dotnet9.Admin/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Dotnet9.Admin
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
