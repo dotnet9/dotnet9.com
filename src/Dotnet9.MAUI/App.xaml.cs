d7f70a76e03d90adc40ded4b0053bbcd9663edba
Dotnet9(dotnet9.com) <1012434131@qq.com>
2022-06-19 23:09:19 +0800

    添加后台模板项目;
    一个共享razor类库，三个后台工程：blazor server、blazor client、maui blazor，计划共用一个razor ui类库，项目都为模板新建项目，先提交，修改时提交记录修改过程；

---
+++ src/Dotnet9.MAUI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Dotnet9.MAUI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ src/Dotnet9.MAUI/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Dotnet9.MAUI
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
