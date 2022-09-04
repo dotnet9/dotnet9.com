849eb5e48d0f164c2c59db631377d545c70287ea
Dotnet9(dotnet9.com) <1012434131@qq.com>
2022-09-04 20:18:58 +0800

    添加swagger配置；

--- src/admin/Dotnet9.WebAPI/Program.cs
+++ src/admin/Dotnet9.WebAPI/Program.cs
@@ -1,3 +1,5 @@
+using Dotnet9.WebAPI.Domain.UserAdmin;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -10,7 +12,35 @@ builder.ConfigureExtraServices(new InitializerOptions
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "Dotnet9.WebAPI", Version = "v1" });
+    //c.AddAuthenticationHeader();
+});
+builder.Services.AddDataProtection();
+//登录、注册的项目除了要启用WebApplicationBuilderExtensions中的初始化之外，还要如下的初始化
+//不要用AddIdentity，而是用AddIdentityCore
+//因为用AddIdentity会导致JWT机制不起作用，AddJwtBearer中回调不会被执行，因此总是Authentication校验失败
+//https://github.com/aspnet/Identity/issues/1376
+IdentityBuilder idBuilder = builder.Services.AddIdentityCore<User>(options =>
+    {
+        options.Password.RequireDigit = false;
+        options.Password.RequireLowercase = false;
+        options.Password.RequireNonAlphanumeric = false;
+        options.Password.RequireUppercase = false;
+        options.Password.RequiredLength = 6;
+        //不能设定RequireUniqueEmail，否则不允许邮箱为空
+        //options.User.RequireUniqueEmail = true;
+        //以下两行，把GenerateEmailConfirmationTokenAsync验证码缩短
+        options.Tokens.PasswordResetTokenProvider = TokenOptions.DefaultEmailProvider;
+        options.Tokens.EmailConfirmationTokenProvider = TokenOptions.DefaultEmailProvider;
+    }
+);
+idBuilder = new IdentityBuilder(idBuilder.UserType, typeof(Role), builder.Services);
+//idBuilder.AddEntityFrameworkStores<IdDbContext>().AddDefaultTokenProviders()
+    //.AddRoleValidator<RoleValidator<Role>>()
+    //.AddRoleManager<RoleManager<Role>>()
+    //.AddUserManager<IdUserManager>();
 
 var app = builder.Build();
 
