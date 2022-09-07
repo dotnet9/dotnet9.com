3bbaf00724c055ecd13ced4b9d985e0f35a6fcd0
Dotnet9(dotnet9.com) <1012434131@qq.com>
2022-09-07 22:52:25 +0800

    基础工程注册成功，接口可正常调用；

---
+++ src/Dotnet9.WebAPI.Infrastructure/ModuleInitializer.cs
@@ -0,0 +1,13 @@
+﻿using Dotnet9.WebAPI.Infrastructure.UserAdmin;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Dotnet9.WebAPI.Infrastructure;
+
+internal class ModuleInitializer : IModuleInitializer
+{
+    public void Initialize(IServiceCollection services)
+    {
+        services.AddScoped<IdDomainService>();
+        services.AddScoped<IIdRepository, IdRepository>();
+    }
+}
