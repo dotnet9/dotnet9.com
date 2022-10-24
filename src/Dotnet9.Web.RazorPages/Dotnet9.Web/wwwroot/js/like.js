efcc1c26302ce5fece20d1a40ecae9bb33ee3716
Dotnet9 <1012434131@qq.com>
2022-10-24 14:37:17 +0800

    Update like.js

--- src/Dotnet9.Web.RazorPages/Dotnet9.Web/wwwroot/js/like.js
+++ src/Dotnet9.Web.RazorPages/Dotnet9.Web/wwwroot/js/like.js
@@ -2,7 +2,7 @@
     $(".diggit").click(function () {
         $.ajax({
             type: "POST",
-            url: "http://localhost:5005/api/blogpost/like/" + $(".blogslug").val(),
+            url: "/api/blogpost/like/" + $(".blogslug").val(),
             contentType: "application/json",
             success: function (res) {
                 $("#diggnum").html(res.data)
