deec9e24dffc880162f6431f36b7b060fc233df7
Dotnet9(dotnet9.com) <1012434131@qq.com>
2023-04-08 18:04:36 +0800

    Successfully debugged the friendship link query interface;

--- src/Dotnet9.WebAPI/src/Services/Dotnet9.Service/Application/FriendlyLinks/Queries/FriendlyLinksQueryValidator.cs
+++ src/Dotnet9.WebAPI/src/Services/Dotnet9.Service/Application/FriendlyLinks/Queries/FriendlyLinksQueryValidator.cs
@@ -4,7 +4,7 @@ public class FriendlyLinksQueryValidator : AbstractValidator<FriendlyLinksQuery>
 {
     public FriendlyLinksQueryValidator()
     {
-        RuleFor(command => command.Page).GreaterThan(1).WithMessage("页码错误");
-        RuleFor(command => command.PageSize).GreaterThan(0).WithMessage("页大小错误");
+        RuleFor(command => command.Page).GreaterThanOrEqualTo(1).WithMessage("页码错误");
+        RuleFor(command => command.PageSize).GreaterThanOrEqualTo(20).WithMessage("页大小错误");
     }
 }
--- src/Dotnet9.WebAPI/src/Services/Dotnet9.Service/Services/FriendlyLinkService.cs
+++ src/Dotnet9.WebAPI/src/Services/Dotnet9.Service/Services/FriendlyLinkService.cs
@@ -2,4 +2,15 @@
 
 public class FriendlyLinkService : ServiceBase
 {
+    public FriendlyLinkService() : base("/api/link")
+    {
+    }
+
+    public async Task<PaginatedListBase<FriendlyLinkDto>> GetAsync(IEventBus eventBus,
+        CancellationToken cancellationToken, string? name, int page = 1, int pageSize = 20)
+    {
+        var friendlyLinkQueryEvent = new FriendlyLinksQuery { Name = name, Page = page, PageSize = pageSize };
+        await eventBus.PublishAsync(friendlyLinkQueryEvent, cancellationToken);
+        return friendlyLinkQueryEvent.Result;
+    }
 }
