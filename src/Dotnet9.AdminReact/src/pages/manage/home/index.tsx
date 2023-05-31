3f77a0e813b05bf8d6b6b21a9168d05132626373
Dotnet9(dotnet9.com) <1012434131@qq.com>
2023-05-31 23:06:37 +0800

    remove Dotnet9.AdminReact;

--- src/Dotnet9.AdminReact/src/pages/manage/home/index.tsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-
-export default class ManageHome extends Component {
-  render() {
-    return (
-      <div>Home</div>
-    )
-  }
-}
--- src/Dotnet9.AdminReact/src/services/userInfoService.ts
+++
@@ -1,7 +0,0 @@
-import request from "../utils/request";
-
-export default class UserInfoService {
-    public static Login(account:string,password:string){
-        return request.post("/api/v1/UserInfoes/Login?account="+account+"&password="+password);
-    }
-}
