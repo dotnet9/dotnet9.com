43f7e60dcf783d806554711827426c9d749e0501
Dotnet9(dotnet9.com) <1012434131@qq.com>
2023-06-16 22:39:56 +0800

    add Dotnet9.BlazorWebApp;

---
+++ src/Dotnet9.BlazorWebApp/Models/WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace Dotnet9.BlazorWebApp;
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public string? Summary { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
