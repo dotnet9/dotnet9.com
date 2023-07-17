6de527b514f27ca777f433bb25d1cb0c7d4610d8
Dotnet9(dotnet9.com) <1012434131@qq.com>
2023-07-17 17:01:35 +0800

    upgrade nuget;

--- src/Dotnet9.Contracts/Dto/Blogs/BlogsSidebarModel.cs
+++ src/Dotnet9.Contracts/Dto/Blogs/BlogsSidebarModel.cs
@@ -1,3 +1,3 @@
 ﻿namespace Dotnet9.Contracts.Dto.Blogs;
 
-public record BlogsSidebarModel(string GroupName, List<BlogBrief> Blogs);
+public record BlogsSidebarModel(string GroupName, List<BlogBrief>? Blogs);
