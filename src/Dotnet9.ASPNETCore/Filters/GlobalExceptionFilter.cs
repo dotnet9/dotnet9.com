8f38d6a17191d8ee0ff338e6d89fff245a36221a
沙漠尽头的狼 <1012434131@qq.com>
2023-08-26 22:11:03 +0800

    update;

--- src/Dotnet9.ASPNETCore/Filters/GlobalExceptionFilter.cs
+++ src/Dotnet9.ASPNETCore/Filters/GlobalExceptionFilter.cs
@@ -1,10 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Dotnet9.ASPNETCore.Filters;
 
-namespace Dotnet9.ASPNETCore.Filters;
-internal class GlobalExceptionFilter
+public class GlobalExceptionFilter : IAsyncExceptionFilter
 {
+    private readonly IHostEnvironment _env;
+    private readonly ILogger<GlobalExceptionFilter> _logger;
+
+    public GlobalExceptionFilter(ILogger<GlobalExceptionFilter> logger, IHostEnvironment env)
+    {
+        _logger = logger;
+        _env = env;
+    }
+
+    public async Task OnExceptionAsync(ExceptionContext context)
+    {
+        var message = _env.IsDevelopment() ? context.Exception.ToString() : "程序中出现未处理异常";
+        var result = new ObjectResult(new
+        {
+            code = (int)HttpStatusCode.InternalServerError,
+            message
+        })
+        {
+            StatusCode = (int)HttpStatusCode.InternalServerError
+        };
+        context.Result = new ObjectResult(ResponseResult<object>.GetError(HttpStatusCode.InternalServerError, message));
+        context.ExceptionHandled = true;
+        _logger.LogError(context.Exception, message);
+        await Task.CompletedTask;
+    }
 }
--- src/Dotnet9.ASPNETCore/Filters/ResultWrapperFilter.cs
+++ src/Dotnet9.ASPNETCore/Filters/ResultWrapperFilter.cs
@@ -1,10 +1,63 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Dotnet9.ASPNETCore.Filters;
 
-namespace Dotnet9.ASPNETCore.Filters;
-internal class ResultWrapperFilter
+public class ResultWrapperFilter : ActionFilterAttribute
+{
+    public override void OnResultExecuting(ResultExecutingContext context)
+    {
+        ControllerActionDescriptor? controllerActionDescriptor = context.ActionDescriptor as ControllerActionDescriptor;
+        object? actionWrapper = controllerActionDescriptor?.MethodInfo
+            .GetCustomAttributes(typeof(NoWrapperAttribute), false).FirstOrDefault();
+        object? controllerWrapper = controllerActionDescriptor?.ControllerTypeInfo
+            .GetCustomAttributes(typeof(NoWrapperAttribute), false).FirstOrDefault();
+        if (actionWrapper != null || controllerWrapper != null)
+        {
+            return;
+        }
+
+        if (context.Result is not ObjectResult objectResult)
+        {
+            return;
+        }
+
+        if (objectResult is BadRequestObjectResult)
+        {
+            if (!context.ModelState.IsValid)
+            {
+                List<string> errors = context.ModelState.Values.Where(v => v.Errors.Count > 0)
+                    .SelectMany(v => v.Errors)
+                    .Select(v => v.ErrorMessage)
+                    .ToList();
+
+                context.Result =
+                    new ObjectResult(ResponseResult<object>.GetError(HttpStatusCode.BadRequest,
+                        errors.JoinAsString(",")));
+            }
+            else
+            {
+                context.Result =
+                    new ObjectResult(ResponseResult<object>.GetResult(false, HttpStatusCode.BadRequest, "",
+                        objectResult.Value));
+            }
+        }
+        else if (objectResult.Value == null)
+        {
+            context.Result = new ObjectResult(ResponseResult<object>.GetError(HttpStatusCode.NotFound, "未找到资源"));
+        }
+        else
+        {
+            if (objectResult.DeclaredType?.IsGenericType == true &&
+                objectResult.DeclaredType?.GetGenericTypeDefinition() ==
+                typeof(ResponseResult<>))
+            {
+                return;
+            }
+
+            context.Result = new ObjectResult(ResponseResult<object>.GetSuccess(objectResult.Value));
+        }
+    }
+}
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, Inherited = false)]
+public class NoWrapperAttribute : Attribute
 {
 }
