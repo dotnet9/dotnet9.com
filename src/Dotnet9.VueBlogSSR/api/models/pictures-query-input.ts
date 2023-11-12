8d6f88f1a6eab6785de9f1c71a5dfe8b6128a5af
沙漠尽头的狼 <632871194@qq.com>
2023-11-12 18:54:09 +0800

    remove vue ssr project;

--- src/Dotnet9.VueBlogSSR/api/models/pictures-query-input.ts
+++
@@ -1,8 +0,0 @@
-import { Pagination } from "./pagination";
-
-export interface PicturesQueryInput extends Pagination {
-  /**
-   * 模块ID
-   */
-  coverId: number;
-}
--- src/Dotnet9.VueBlogSSR/pages/article/tags.page.route.ts
+++
@@ -1,1 +0,0 @@
-export default "/tags/@tid";
